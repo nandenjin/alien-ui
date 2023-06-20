1412e66b8240c2e5d7eac6d560331d173635f949
Kazumi Inada <hello@nandenjin.com>
2023-06-20 13:39:48 +0900

    chore: Separate ESLint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,30 +4,7 @@ module.exports = {
     node: true,
   },
   parser: 'vue-eslint-parser',
-  plugins: ['vue', 'prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    'plugin:vue/recommended',
-    'prettier/@typescript-eslint',
-    'prettier/vue',
-  ],
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        semi: false,
-        singleQuote: true,
-        overrides: [
-          {
-            files: '*.pug',
-            options: {
-              parser: 'pug',
-              singleQuote: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
+  plugins: ['vue'],
+  extends: ['eslint:recommended', 'plugin:vue/recommended'],
+  rules: {},
 }
